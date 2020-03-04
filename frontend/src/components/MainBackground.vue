24c98bc4678beed5b1ef1b519f8b338948c5360f
Connor Hill <chill0594@gmail.com>
2020-03-03 20:15:59 -0500

    first commit

---
+++ frontend/src/components/MainBackground.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="h-screen">
+    <div style="height: 100vw;" class="transform rotate-45">
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/greenRect.svg" alt="greensvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/whiteRect.svg" alt="whitesvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/greenRect.svg" alt="greensvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/purpleRect.svg" alt="purplesvg" class="shadow inline" />
+        <img src="../../public/svgs/whiteRect.svg" alt="whitesvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/whiteRect.svg" alt="whitesvg" class="shadow inline" />
+        <img src="../../public/svgs/purpleRect.svg" alt="purplesvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/greenRect.svg" alt="greensvg" class="shadow inline" />
+        <img src="../../public/svgs/whiteRect.svg" alt="whitesvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/whiteRect.svg" alt="whitesvg" class="shadow inline" />
+        <img src="../../public/svgs/greenRect.svg" alt="greensvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/purpleRect.svg" alt="purplesvg" class="shadow inline" />
+        <img src="../../public/svgs/whiteRect.svg" alt="whitesvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/greenRect.svg" alt="greensvg" class="shadow inline" />
+      </div>
+      <div class="flex mb-20 justify-center">
+        <img src="../../public/svgs/whiteRect.svg" alt="whitesvg" class="shadow inline" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainBackground"
+};
+</script>
+
+<style></style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <MainBackground />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainBackground from "@/components/MainBackground.vue";
+
+export default {
+  name: "Home",
+  components: {
+    MainBackground
+  }
+};
+</script>
